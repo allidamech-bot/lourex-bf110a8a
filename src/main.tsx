2420f9a533b8c14632306c25e1746780ad89b4a5
ALI M.DAAMISH <allidamech@gmail.com>
2026-05-21 19:49:19 +0300

    Load purchase request hardening stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "next-themes";
 import App from "./App.tsx";
 import "./index.css";
+import "./styles/purchase-requests-hardening.css";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
