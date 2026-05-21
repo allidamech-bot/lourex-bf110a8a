43a027ae9201ddc2b6223fa9339ba08ee1e9be9b
ALI M.DAAMISH <allidamech@gmail.com>
2026-05-21 03:02:30 +0300

    Add operations briefing dashboard page

---
+++ src/pages/dashboard/OperationsBriefingPage.tsx
@@ -0,0 +1,5 @@
+import { OperationsBriefingWidget } from "@/features/operations/OperationsBriefingWidget";
+
+const OperationsBriefingPage = () => <OperationsBriefingWidget />;
+
+export default OperationsBriefingPage;
